ced87d7db48ec9a2d700e8b7f66318a4824e361b
Marko Mihelj <marko.mihelj@infinum.hr>
2021-05-25 16:07:12 +0200

    fix multilingual support

--- pages/[furniture].jsx
+++ pages/[furniture].jsx
@@ -4,14 +4,14 @@ import { Navbar } from "../components/shared/Navbar/Navbar";
 import { Footer } from "../components/shared/Footer/Footer";
 import { Meta } from "../components/shared/Meta/Meta";
 
-import { FurniturePreview } from "../components/furniture/FurniturePreview/FurniturePreview";
+// import { FurniturePreview } from "../components/furniture/FurniturePreview/FurniturePreview";
 
 const FurniturePage = () => {
   return (
     <>
       <Meta />
       <Navbar />
-      <FurniturePreview />
+      {/* <FurniturePreview /> */}
       <Footer />
     </>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,14 @@ import { Navbar } from "../components/shared/Navbar/Navbar";
 import { Footer } from "../components/shared/Footer/Footer";
 import { Meta } from "../components/shared/Meta/Meta";
 
-import { Jumbotron } from "../components/home/Jumbotron/Jumbotron";
+// import { Jumbotron } from "../components/home/Jumbotron/Jumbotron";
 
 const Home = () => {
   return (
     <>
       <Meta />
       <Navbar />
-      <Jumbotron />
+      {/* <Jumbotron /> */}
       <Footer />
     </>
   );
